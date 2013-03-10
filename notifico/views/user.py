25aaddbbdd58ada93f0dd9c2afaf6ee767ca0099
Tyler Kennedy <tk@tkte.ch>
2013-03-09 20:26:05 -0500

    Added signup page mockup.

--- notifico/views/user.py
+++ notifico/views/user.py
@@ -5,7 +5,7 @@ from flask.ext import wtf
 from notifico import app
 
 
-class UserLoginForm(wtf.Form):
+class LoginForm(wtf.Form):
     username = wtf.TextField('Username', validators=[
         wtf.Required()
     ])
@@ -14,10 +14,41 @@ class UserLoginForm(wtf.Form):
     ])
 
 
+class SignUpForm(wtf.Form):
+    username = wtf.TextField('Username', validators=[
+        wtf.Required(),
+        wtf.Length(min=2, max=50),
+        wtf.Regexp('^[a-zA-Z0-9_]*$', message=(
+            'Username must only contain a to z, 0 to 9, and underscores.'
+        ))
+    ], description=(
+        'Your username is public and used as part of your project name.'
+    ))
+    email = wtf.TextField('Email', validators=[
+        wtf.Required(),
+        wtf.validators.Email()
+    ])
+    password = wtf.PasswordField('Password', validators=[
+        wtf.Required(),
+        wtf.Length(5),
+        wtf.EqualTo('confirm', 'Passwords do not match.'),
+    ])
+    confirm = wtf.PasswordField('Confirm Password')
+
+
 @app.route('/user/login', methods=['GET', 'POST'])
 def user_login():
     """
     Present the user with a login form, and authenticate them on POST.
     """
-    login_form = UserLoginForm()
+    login_form = LoginForm()
     return render_template('user/login.jinja', login_form=login_form)
+
+
+@app.route('/user/signup', methods=['GET', 'POST'])
+def user_signup():
+    """
+    Present the user with a sign up form, creating an account on POST.
+    """
+    signup_form = SignUpForm()
+    return render_template('user/register.jinja', signup_form=signup_form)
