68f95fc2ad11e36a45af350ab99eb055fd686129
Tyler Kennedy <tk@tkte.ch>
2013-03-12 17:59:42 -0400

    More changes for a fluid dashboard.

--- notifico/views/misc.py
+++ notifico/views/misc.py
@@ -1,10 +1,16 @@
 # -*- coding: utf8 -*-
 from flask import (
+    g,
+    redirect,
+    url_for,
     render_template
 )
-from notifico import app, user_required
+from notifico import app
 
 
 @app.route('/')
 def misc_landing():
+    if g.user:
+        return redirect(url_for('projects_dashboard'))
+
     return render_template('misc/landing.jinja')
