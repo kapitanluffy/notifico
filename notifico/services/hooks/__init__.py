33e2cf708bdca1fda97050f95a9039c62c95505a
Tyler Kennedy <tk@tkte.ch>
2013-03-18 19:23:07 -0400

    Use twitter typeahead for project searching.

---
+++ notifico/services/hooks/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+from notifico.services.hooks.hook import *
+from notifico.services.hooks.bitbucket import *
+from notifico.services.hooks.github import *
+from notifico.services.hooks.hook import *
+from notifico.services.hooks.plain import *
+from notifico.services.hooks.jira import *
+from notifico.services.hooks.cia import *
--- notifico/views/projects.py
+++ notifico/views/projects.py
@@ -1,8 +1,11 @@
 # -*- coding: utf8 -*-
+import json
+
 from flask import (
     g,
     request,
-    render_template
+    render_template,
+    Response
 )
 
 from notifico import app, user_required
@@ -12,13 +15,36 @@ from notifico.models import Project
 @app.route('/dashboard')
 @user_required
 def projects_dashboard():
-    per_page = min(int(request.args.get('l', 10)), 100)
+    per_page = min(int(request.args.get('l', 20)), 100)
     page = max(int(request.args.get('page', 1)), 1)
+    q = request.args.get('q')
 
-    projects = (
-        g.user.projects
-        .order_by(False)
-        .order_by(Project.created.desc())
-    ).paginate(page, per_page, False)
+    # Search starting from all this users projects....
+    projects = g.user.projects
+    if q:
+        # ... and if a search query was provided, filter down on that...
+        projects = projects.filter(Project.name.like(q + '%'))
+
+    projects = projects.order_by(False).order_by(Project.created.desc())
+    projects = projects.paginate(page, per_page, False)
 
     return render_template('projects/dashboard.jinja', projects=projects)
+
+
+@app.route('/_/private_search')
+@user_required
+def projects_private_search():
+    q = request.args.get('q')
+
+    results = [{
+        'value': p.name,
+        'tokens': [p.name]
+    } for p in g.user.projects if p.name.lower().startswith(q.lower())]
+
+    return Response(json.dumps(results),  mimetype='application/json')
+
+
+@app.route('/new')
+@user_required
+def projects_new():
+    pass
