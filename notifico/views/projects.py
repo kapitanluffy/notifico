416f6a2bdf58df5a609cd73b11ea4dca0c957667
Tyler Kennedy <tk@tkte.ch>
2013-03-18 22:21:33 -0400

    Improve projects_private_search() query; sorting on projects_dashboard searches.

--- notifico/views/projects.py
+++ notifico/views/projects.py
@@ -15,18 +15,29 @@ from notifico.models import Project
 @app.route('/dashboard')
 @user_required
 def projects_dashboard():
+    """
+    Presents the user with an overview of their projects and activity.
+    """
     per_page = min(int(request.args.get('l', 20)), 100)
     page = max(int(request.args.get('page', 1)), 1)
+    sort_by = request.args.get('s', 'created')
     q = request.args.get('q')
 
-    # Search starting from all this users projects....
-    projects = g.user.projects
+    # Search starting from all this user's projects....
+    projects_q = g.user.projects
     if q:
         # ... and if a search query was provided, filter down on that...
-        projects = projects.filter(Project.name.like(q + '%'))
+        projects_q = projects_q.filter(Project.name.like(q + '%'))
 
-    projects = projects.order_by(False).order_by(Project.created.desc())
-    projects = projects.paginate(page, per_page, False)
+    # ... then clear any model-set sorting and sort by whatever
+    #     was selected.
+    projects_q = projects_q.order_by(False).order_by({
+        'created': Project.created.desc(),
+        'messages': Project.message_count.desc()
+    }.get(sort_by, Project.created.desc()))
+
+    # ... and finally generate the Flask-SQLAlchemy Pagination() object.
+    projects = projects_q.paginate(page, per_page, False)
 
     return render_template('projects/dashboard.jinja', projects=projects)
 
@@ -34,12 +45,23 @@ def projects_dashboard():
 @app.route('/_/private_search')
 @user_required
 def projects_private_search():
-    q = request.args.get('q')
+    """
+    Searches for projects starting with the request argument `q`, returning
+    a JSON list of [{value: ..., tokens: ...}]. This view only returns
+    projects for the currently logged in user.
+    """
+    q = request.args.get('q', '')
+    per_page = min(int(request.args.get('l', 5)), 25)
+    page = min(int(request.args.get('page', 1)), 100)
 
-    results = [{
-        'value': p.name,
-        'tokens': [p.name]
-    } for p in g.user.projects if p.name.lower().startswith(q.lower())]
+    projects_q = (
+        g.user.projects
+        .filter(Project.name.like(q + '%'))
+        .offset((page - 1) * per_page)
+        .limit(per_page)
+    )
+
+    results = [dict(value=p.name, tokens=[p.name]) for p in projects_q]
 
     return Response(json.dumps(results),  mimetype='application/json')
 
